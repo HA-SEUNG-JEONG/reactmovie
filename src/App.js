bd2f5ca6a470f293fe7dcd6b3e013ae6023e6ddf
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2021-11-26 09:40:24 +0900

    To Do List Part Two

--- src/App.js
+++ src/App.js
@@ -9,9 +9,11 @@ function App() {
     if (todo === "") {
       return;
     }
-    setTodo(""); //todo 값이 비어있지 않다면 Form 제출 후 input 값 비워버림
     setTodos((currentArray) => [todo, ...currentArray]); //기존 todo에 currentArray 배열의 원소들을 집어넣어서 새로운 array를 만들고자 할 때 ...를 앞에다 붙이면 된다.
+    setTodo(""); //todo 값이 비어있지 않다면 Form 제출 후 input 값 비워버림
   };
+  console.log(todos);
+  console.log(todos.map((item, index) => <li key={index}>{item}</li>));
   return (
     <div>
       <h1>My To Dos ({todos.length})</h1>
@@ -24,6 +26,12 @@ function App() {
         />
         <button>Add To do</button>
       </form>
+      <hr />
+      <ul>
+        {todos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
     </div>
   );
 }
